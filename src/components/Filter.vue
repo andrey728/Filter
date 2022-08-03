240ee925a58dae5440eb3cd2a235e72fb6f53152
Andrey_728 <Andrey_728@mail.ru>
2022-08-03 22:09:43 +0500

    some test comitsd

---
+++ src/components/Filter.vue
@@ -0,0 +1,67 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+import Form from "../components/Form.vue";
+
+export default {
+  name: "Filter",
+  components: {Form},
+  props: {
+    filterId: {
+      type: String,
+      default: ""
+    },
+    filterType: {
+      type: String,
+      default: ""
+    }
+  }
+}
+</script>
+
+
+<template>
+
+  <div class="Card" v-bind:id="filterId" v-if="filterType==='number'">
+    <Form
+        placeholder="Больше"
+        inputType="number"
+        @on-enter="onEnter"
+    ></Form>
+    <Form
+        placeholder="Меньше"
+        inputTypr="number"
+    ></Form>
+    <Form
+        placeholder="Равен"
+        inputTypr="number"
+    ></Form>
+    <Form
+        placeholder="Не равен"
+        inputTypr="number"
+    ></Form>
+  </div>
+
+
+  <div class="Card" v-bind:id="filterId" v-if="filterType==='string'">
+    <Form
+        placeholder="Содержит"
+        inputType="text"
+    ></Form>
+    <Form
+        placeholder="Не содержит"
+        inputType="text"
+    ></Form>
+  </div>
+
+  <div class="Card" v-bind:id="filterId" v-if="filterType==='bool'">
+    <Form
+        inputType="checkbox"
+    ></Form>
+  </div>
+
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/Form.vue
@@ -0,0 +1,55 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+
+
+export default defineComponent({
+  name: "TextField",
+  props: {
+    textValue: {
+      type: String,
+      default: '',
+    },
+    placeholder: {
+      type: String,
+      default: 'Поиск'
+    },
+    inputType: {
+      type: String,
+      default: "number"
+    }
+  },
+  emits: ['update:textValue', 'onEnter'],
+  setup(props, {emit}) {
+    const onEnter = () => {
+      emit('onEnter');
+    }
+
+    const searchText = (event: any) => {
+      emit('update:textValue', event.target.value);
+    }
+
+    return {
+      onEnter,
+      searchText,
+    }
+  }
+})
+</script>
+
+<style scoped>
+
+</style>
+
+<template>
+
+
+  <input
+      class="cherecters"
+      :type="inputType"
+
+      :value="textValue"
+      @input="searchText"
+      @keydown.enter="onEnter"
+      :placeholder="placeholder"
+  />
+</template>
