75fe7ae905e798b207f93a1aa3ca9cd45e8a8a72
Andrey_728 <Andrey_728@mail.ru>
2022-10-11 11:46:20 +0500

    validators in store jats worked, request_obj has been changed

--- src/Store.js
+++ src/Store.js
@@ -1,12 +1,21 @@
 import { createApp } from 'vue'
 import { createStore } from 'vuex'
 
+//const minLength = num => val => val.length >= num
+const minLength = (num, val) => {
+    return (val.length >= num)
+}
+
+const correctRange = (gt, lt) => {
+    return !((gt - lt) >= 0)
+}
+
 // Create a new store instance.
 const store = createStore({
     state: {
         inputed_json: [],
-        recuest_objects: [],
-        count: 0
+        recuest_objects: {},
+        request_validators: {}
     },
     mutations: {
         SET_INPUTED_JSON_TO_STATE: (state, inputed_json) => {
@@ -14,61 +23,78 @@ const store = createStore({
         },
 
         GENERATE_RECURSE_OBJ: (state, field) => {
-            console.log(field)
+            state.request_validators[field.code] = {
+                errors: null,
+                select_range: false,
+                minLength: {}
+            }
+
             if (field.type === 'number') {
-                state.recuest_objects.push({
-                    code: field.code,
-                    values: {
-                        GT: null,
-                        LT: null,
-                        GTE: null,
-                        LTE: null,
-                        EQ: null,
-                        NEQ: null
-                    }
-                });
+                state.recuest_objects[field.code] = {
+                    GT: null,
+                    LT: null,
+                    GTE: null,
+                    LTE: null,
+                    EQ: null,
+                    NEQ: null
+                }
+
+
             }
             if (field.type === 'string') {
-                state.recuest_objects.push({
-                    code: field.code,
-                    values: {
-                        Like: null,
-                        NEQ: null
-                    }
-                });
+                state.recuest_objects[field.code] = {
+                    Like: null,
+                    NEQ: null
+                }
+
             }
             if (field.type === 'bool') {
-                state.recuest_objects.push({
-                    code: field.code,
-                    values: {
-                        EQ: null
-                    }
-                });
+                state.recuest_objects[field.code] = {
+                    EQ: null
+                }
+
             }
+            console.log('gen', state.recuest_objects)
         },
 
         REFRESH: (state) => {
             state.recuest_objects = []
             state.inputed_json = []
         },
-        NEW_COUNT_TO_STATE: (state, new_count) => {
-            state.count = new_count;
-        },
 
-        FILTER_FIELD: (state, payload) => {
-            for (const i in state.recuest_objects) {
-                if (state.recuest_objects[i].code === payload.code) {
-                    state.recuest_objects[i].values[payload.params] = payload.value
-                    //return state.recuest_objects[i].values;
-                }
+        VALIDATORS_CHENGES: (state, payload) => {
+            // отметка заполняемости
+            state.request_validators[payload.code].minLength[payload.params] = minLength(
+                1, state.recuest_objects[payload.code][payload.params]
+            )
+            // отметка заполнения диапозона
+            state.request_validators[payload.code].select_range = !!(
+                state.request_validators[payload.code].minLength['GT']
+                &
+                state.request_validators[payload.code].minLength['LT']
+            )
+            console.log(state.request_validators[payload.code])
+            // проверка корявости диапозона
+            if(state.request_validators[payload.code].select_range) {
+                console.log('ща чекать будем')
+                console.log(state.recuest_objects[payload.code])
+                state.request_validators[payload.code].errors = !correctRange(
+                    state.recuest_objects[payload.code]['GT'],
+                    state.recuest_objects[payload.code]['LT']
+                )
+                console.log(state.request_validators[payload.code].errors)
             }
         },
 
+        FILTER_FIELD: (state, payload) => {
+            state.recuest_objects[payload.code][payload.params] = payload.value
+            store.commit('VALIDATORS_CHENGES', payload)
+
+        },
+
         TAKE (state, code) {
-            console.log(code)
             for (const i in state.recuest_objects) {
                 if (state.recuest_objects[i].code === code) {
-                    console.log('гетер', state.recuest_objects[i].values)
                     return state.recuest_objects[i].values;
                 }
             }
@@ -78,13 +104,11 @@ const store = createStore({
 
     actions: {
         INPUT_JSON({comit}, inputed_json) {
-            comit('SET_INPUTED_JSON_TO_STATE', inputed_json.data);
-            console.log(inputed_json.data)
+            commit('SET_INPUTED_JSON_TO_STATE', inputed_json.data);
             return inputed_json;
         },
         COUNT_CHANGE({comit}, new_count) {
-            comit('NEW_COUNT_TO_STATE', new_count);
-            console.log("yep")
+            commit('NEW_COUNT_TO_STATE', new_count);
             return new_count;
         }
     },
